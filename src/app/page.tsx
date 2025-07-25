ee19f1a4d364e09f4af64e455ec216cd20012211
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2025-07-25 19:19:28 +0530

    add a footer with links and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,23 @@
 import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Send, Loader2Icon } from "lucide-react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Send, Loader2Icon, Sun, MoonStar } from "lucide-react";
 import { getDetails } from "@/app/api/client";
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function Page() {
   const [discordID, setDiscordID] = useState<string>("");
   const [loading, setLoading] = useState(false);
+  const { setTheme } = useTheme();
 
   const isValidID = (idString: string) => {
     const regex = /^\d{17,20}$/;
@@ -24,39 +34,92 @@ export default function Page() {
   };
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col row-start-2 items-center sm:items-start">
-        <div className="flex flex-col items-center">
-          <h1 className="text-3xl sm:text-5xl font-bold">Discord ID Lookup</h1>
-          <p className="mt-2">
-            Enter a Discord user/guild/message ID to get user information
-          </p>
-        </div>
-        <Card className="w-full max-w-md mt-4">
-          <CardContent>
-            <div className="flex gap-2">
-              <Input
-                placeholder="Enter Discord ID"
-                onChange={(e) => setDiscordID(e.target.value)}
-                value={discordID}
-                className=""
-              />
-              <Button
-                variant="default"
-                size={"icon"}
-                disabled={!isValidID(discordID) || loading}
-                onClick={handleLookup}
-              >
-                {loading ? (
-                  <Loader2Icon className="animate-spin" />
-                ) : (
-                  <Send className="size-4" />
-                )}
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20">
+      <div className="flex flex-col items-center">
+        <main className="flex flex-col items-center sm:items-start">
+          <div className="flex flex-col items-center">
+            <h1 className="text-3xl sm:text-5xl font-bold">
+              Discord ID Lookup
+            </h1>
+            <p className="mt-2">
+              Enter a Discord user/guild/message ID to get user information
+            </p>
+          </div>
+          <Card className="w-full max-w-md mt-4">
+            <CardContent>
+              <div className="flex gap-2">
+                <Input
+                  placeholder="Enter Discord ID"
+                  onChange={(e) => setDiscordID(e.target.value)}
+                  value={discordID}
+                  className=""
+                />
+                <Button
+                  variant="default"
+                  size={"icon"}
+                  disabled={!isValidID(discordID) || loading}
+                  onClick={handleLookup}
+                >
+                  {loading ? (
+                    <Loader2Icon className="animate-spin" />
+                  ) : (
+                    <Send className="size-4" />
+                  )}
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        </main>
+
+        <footer className="mt-6 text-right flex justify-end gap-2 w-full max-w-md">
+          <Link
+            target="_blank"
+            href="https://discord.com/servers/uncover-it-1298592315694387220"
+            className={buttonVariants({ variant: "outline", size: "icon" })}
+          >
+            <Image
+              src="/discord.svg"
+              alt="Discord"
+              width="19"
+              height="19"
+              className="dark:brightness-100 brightness-0"
+            />
+          </Link>
+          <Link
+            target="_blank"
+            href="https://github.com/WarFiN123/webhook-multitool"
+            className={buttonVariants({ variant: "outline", size: "icon" })}
+          >
+            <Image
+              src="/github.svg"
+              alt="GitHub"
+              width="19"
+              height="19"
+              className="dark:brightness-100 brightness-0"
+            />
+          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <MoonStar className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
               </Button>
-            </div>
-          </CardContent>
-        </Card>
-      </main>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </footer>
+      </div>
     </div>
   );
 }
